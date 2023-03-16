1a305088f203d3b9109f49354e43fecc19015144
Dylan Chamberlain <Dylan.chambo@gmail.com>
2023-03-17 11:47:01 +1300

    feat: :sparkles: complete lab 1

--- Lab1/Wave Gen/wgen1.vhd
+++ Lab1/Wave Gen/wgen1.vhd
@@ -1,10 +1,11 @@
-entity wgen1 is
+entity wgen2 is
   port (X : in bit_vector(2 downto 0);
+        Enable: in bit;
         A, B, C, Z : out bit);
-end entity wgen1;
+end entity wgen2;
 
-architecture behaviour of wgen1 is
-  signal v_A, v_B, v_C : bit;
+architecture behaviour of wgen2 is
+  signal v_A, v_B, v_C, v_Z : bit;
 begin
   sigGen: process
   begin
@@ -16,17 +17,18 @@ begin
   process(X, v_A, v_B, v_C)
   begin
     case X is
-      when "000" => Z <= v_A and v_B;
-      when "001" => Z <= v_B or v_C;
-      when "010" => Z <= (v_A or v_B) and v_C;
-      when "011" => Z <= (v_A xor v_B) or v_C;
-      when "100" => Z <= v_A;
-      when "101" => Z <= v_B;
-      when "110" => Z <= v_C;
-      when "111" => Z <= '1';
+      when "000" => v_Z <= v_A and v_B;
+      when "001" => v_Z <= v_B or v_C;
+      when "010" => v_Z <= (v_A or v_B) and v_C;
+      when "011" => v_Z <= (v_A xor v_B) or v_C;
+      when "100" => v_Z <= v_A;
+      when "101" => v_Z <= v_B;
+      when "110" => v_Z <= v_C;
+      when "111" => v_Z <= '1';
     end case;
   end process;
-  A <= v_A;
-  B <= v_B;
-  C <= v_C;
+  A <= v_A and Enable;
+  B <= v_B and Enable;
+  C <= v_C and Enable;
+  Z <= v_Z and Enable;
 end architecture behaviour;
